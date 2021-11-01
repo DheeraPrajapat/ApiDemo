d4a3b6a4f0503c750713c41550f12d7e18a1243d
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-11-01 18:18:54 +0530

    Update the funcationality and activity name

--- app/src/main/java/com/example/apidemo/Activities/WelcomeActivity.java
+++ app/src/main/java/com/example/apidemo/Activities/WelcomeActivity.java
@@ -1,4 +1,4 @@
-package com.example.apidemo;
+package com.example.apidemo.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,22 +7,27 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.apidemo.R;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
         Handler handler=new Handler();
         handler.postDelayed(() -> {
             SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
             String status = sh.getString("status", "");
             if(status.equalsIgnoreCase("login")) {
-                startActivity(new Intent(WelcomeActivity.this,MainActivity.class)
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class)
                         .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
                 finish();
-            } startActivity(new Intent(WelcomeActivity.this,SignInActivity.class)
+            } startActivity(new Intent(WelcomeActivity.this, SignInActivity.class)
                     .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
             finish();
         },1200);
