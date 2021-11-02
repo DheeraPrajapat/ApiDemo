82f562822cf9612ad753a017f37628bc73978ce3
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-11-02 16:25:54 +0530

    add new option in options menu

--- app/src/main/java/com/example/apidemo/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/apidemo/Activities/ProfileActivity.java
@@ -1,9 +1,17 @@
 package com.example.apidemo.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +20,7 @@ import android.widget.Toast;
 import com.example.apidemo.Package.ApiClient;
 import com.example.apidemo.R;
 import com.example.apidemo.Service.UserService;
+import com.example.apidemo.SignUpPojo.ChangePasswordModel;
 import com.example.apidemo.SignUpPojo.GetProfileModel;
 import com.example.apidemo.SignUpPojo.Model;
 
@@ -24,15 +33,21 @@ public class ProfileActivity extends AppCompatActivity {
     Button editBtn,updateBtn;
     UserService userService;
     String token,id;
-
+    ProgressDialog progressDialog;
+    AlertDialog.Builder alert;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        SharedPreferences sharedPreferences=getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        token=sharedPreferences.getString("token","");
+        id=sharedPreferences.getString("id","");
+
+        Log.d("info","token :-"+token);
+        Log.d("info","id :-"+id);
         initViews();
         getAllInformationOfUser();
         editBtn.setOnClickListener(v->{
-            email.setEnabled(true);
             dType.setEnabled(true);
             region.setEnabled(true);
             updateBtn.setVisibility(View.VISIBLE);
@@ -40,12 +55,40 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
         updateBtn.setOnClickListener(v->{
-            email.setEnabled(false);
-            dType.setEnabled(false);
-            region.setEnabled(false);
-            updateBtn.setVisibility(View.GONE);
-            editBtn.setVisibility(View.VISIBLE);
+            String device =dType.getText().toString();
+            String Region=region.getText().toString();
+            if(device.equalsIgnoreCase("") || Region.equalsIgnoreCase("")){
+                Toast.makeText(ProfileActivity.this, "Field cannot be blank...", Toast.LENGTH_SHORT).show();
+            }else {
+                updateTheUserData(device,Region);
+            }
         });
+    }
+
+    private void updateTheUserData(String device, String address)
+    {
+        progressDialog.show();
+        userService.callUpdateTheUserInformation(address,device).enqueue(new Callback<GetProfileModel>() {
+            @Override
+            public void onResponse(Call<GetProfileModel> call, Response<GetProfileModel> response) {
+                if(response.isSuccessful()){
+                    progressDialog.dismiss();
+                    dType.setEnabled(false);
+                    region.setEnabled(false);
+                    updateBtn.setVisibility(View.GONE);
+                    editBtn.setVisibility(View.VISIBLE);
+                    getAllInformationOfUser();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GetProfileModel> call, Throwable t) {
+                progressDialog.dismiss();
+                alert.setMessage(t.getMessage());
+                alert.setPositiveButton("Ok", (dialog, which) -> Toast.makeText(ProfileActivity.this, "Thank you...", Toast.LENGTH_SHORT).show());
+            }
+        });
+
     }
 
     private void initViews()
@@ -56,10 +99,10 @@ public class ProfileActivity extends AppCompatActivity {
         region=findViewById(R.id.profileRegion);
         editBtn=findViewById(R.id.editProfile);
         updateBtn=findViewById(R.id.updateProfile);
-        SharedPreferences sharedPreferences=getSharedPreferences("MySharedPref", MODE_PRIVATE);
-        token=sharedPreferences.getString("token","");
-        id=sharedPreferences.getString("id","");
         userService= ApiClient.getClientTokn(token).create(UserService.class);
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setMessage("Please wait , we are update your information...");
+        alert=new AlertDialog.Builder(this);
     }
 
     private void getAllInformationOfUser(){
@@ -73,7 +116,7 @@ public class ProfileActivity extends AppCompatActivity {
                     userId.setText(id);
                     region.setText(profileModel.body.getAddress());
                 }else{
-                    Toast.makeText(ProfileActivity.this, "Error occured...", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ProfileActivity.this, "Error occured :-"+response.errorBody().toString(), Toast.LENGTH_SHORT).show();
                 }
             }
             @Override
@@ -91,5 +134,59 @@ public class ProfileActivity extends AppCompatActivity {
         userId.setEnabled(false);
         region.setEnabled(false);
         updateBtn.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.change_password){
+            ChangePassword();
+            return true;
+        }
+        return false;
+    }
+
+    private void ChangePassword()
+    {
+        AlertDialog alertDialog=new AlertDialog.Builder(this).create();
+        View view= LayoutInflater.from(this).inflate(R.layout.change_password,null,false);
+        alertDialog.setView(view);
+        alertDialog.show();
+        Button submitBtn,cancelBtn;
+        EditText oldPs,newPs;
+        submitBtn=view.findViewById(R.id.submitButton);
+        cancelBtn=view.findViewById(R.id.cancelButton);
+        oldPs=view.findViewById(R.id.oldPassword);
+        newPs=view.findViewById(R.id.newPassword);
+        submitBtn.setOnClickListener(v -> {
+            String old,New;
+            old=oldPs.getText().toString();
+            New=newPs.getText().toString();
+            if(old.equals("") || New.equals("")){
+                Toast.makeText(ProfileActivity.this, "Fill the password...", Toast.LENGTH_SHORT).show();
+            }
+            userService.callChangePassword(old,New).enqueue(new Callback<ChangePasswordModel>() {
+                @Override
+                public void onResponse(Call<ChangePasswordModel> call, Response<ChangePasswordModel> response) {
+                    if(response.isSuccessful()){
+                        alertDialog.dismiss();
+                        Toast.makeText(ProfileActivity.this, "Password changed successfully...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                @Override
+                public void onFailure(Call<ChangePasswordModel> call, Throwable t) {
+                    alertDialog.dismiss();
+                    Toast.makeText(ProfileActivity.this, "Error occured :- "+t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            cancelBtn.setOnClickListener(v1 -> alertDialog.dismiss());
+        });
+
     }
 }
