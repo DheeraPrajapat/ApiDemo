a96b83ee557718e5af8c949666642647caaedeb2
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-11-11 12:53:05 +0530

    Create two methods for get comments and post comment

--- app/src/main/java/com/example/apidemo/Service/UserService.java
+++ app/src/main/java/com/example/apidemo/Service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.apidemo.Service;
 
+import com.example.apidemo.PojoClasses.CreatePostComment.CommentModel;
+import com.example.apidemo.PojoClasses.GetAllComments.GetAllCommentsModel;
 import com.example.apidemo.PojoClasses.GetPost.GetAllPostModel;
 import com.example.apidemo.PojoClasses.SearchUser.SearchModel;
 import com.example.apidemo.SignUpPojo.ChangePasswordModel;
@@ -70,4 +72,13 @@ public interface UserService
     @FormUrlEncoded
     @POST("like")
     Call<LikeModel> callbackLike(@Field("feed_id") int feed_id , @Field("user_id") int user_id , @Field("status") int status);
+
+    @FormUrlEncoded
+    @POST("add-feed-comment")
+    Call<CommentModel> callbackPostComment(@Field("feed_id") int feed_id, @Field("commenter_id") int commenter_id , @Field("comment") String comment);
+
+    @FormUrlEncoded
+    @POST("get-feed-comments")
+    Call<GetAllCommentsModel> callbackGetAllComments(@Field("feed_id") int feed_id , @Field("page") int page ,@Field("limit") int limit);
+
 }
