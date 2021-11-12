3e7145d147c06b72009dcded5e4847c2c8a77bc2
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-11-12 12:30:04 +0530

    Create new interface for get the informartion of teams

---
+++ app/src/main/java/com/example/apidemo/Service/TeamService.java
@@ -0,0 +1,22 @@
+package com.example.apidemo.Service;
+
+import com.example.apidemo.PojoClasses.CreatePost.UserPostModel;
+import com.example.apidemo.Team.GetAllTeams.GetAllTeamModel;
+import com.example.apidemo.Team.UserTeamPojo.UserTeamBody;
+import com.example.apidemo.Team.UserTeamPojo.UserTeamModel;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface TeamService
+{
+    @FormUrlEncoded
+    @POST("get-user-team")
+    Call<UserTeamModel> callbackGetUserTeam(@Field("user_id") String userId);
+
+    @GET("get-all-teams")
+    Call<GetAllTeamModel> callbackGetAllTeam();
+}
