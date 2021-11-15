57f993bc9430629b5c71b45b70684e983d68fdea
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-11-15 16:15:36 +0530

    define the function of update team and delete team , getUserTeam

--- app/src/main/java/com/example/apidemo/Service/TeamService.java
+++ app/src/main/java/com/example/apidemo/Service/TeamService.java
@@ -2,7 +2,9 @@ package com.example.apidemo.Service;
 
 import com.example.apidemo.PojoClasses.CreatePost.UserPostModel;
 import com.example.apidemo.Team.CreateTeamPojo.CreateTeamModel;
+import com.example.apidemo.Team.DeleteTeamPojo.DeleteModel;
 import com.example.apidemo.Team.GetAllTeams.GetAllTeamModel;
+import com.example.apidemo.Team.UpdateTeamInfo.UpdateModel;
 import com.example.apidemo.Team.UserTeamPojo.UserTeamBody;
 import com.example.apidemo.Team.UserTeamPojo.UserTeamModel;
 
@@ -24,4 +26,12 @@ public interface TeamService
     @FormUrlEncoded
     @POST("create-team")
     Call<CreateTeamModel> callbackCreateTeam(@Field("title") String title, @Field("admin_id") int admin_id , @Field("description") String description);
+
+    @FormUrlEncoded
+    @POST("Delete-team")
+    Call<DeleteModel> callDeleteTeam(@Field("team_id") String teamId, @Field("admin_id") int admin_id);
+
+    @FormUrlEncoded
+    @POST("update-team")
+    Call<UpdateModel> callUpdateTeam(@Field("team_id") int team_id , @Field("title") String title , @Field("description") String description);
 }
